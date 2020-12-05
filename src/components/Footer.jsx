c03edc933eeebea2d9cf8292d5fa8d73dfa4ad11
Akif Kartal <akokartal@gmail.com>
2020-12-05 12:31:05 +0300

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Container } from 'reactstrap';
+import React from "react";
+import { Container } from "reactstrap";
 
 const Footer = (props) => {
-    return (
-        <footer className="text-muted">
-            <Container>
-                <p className="float-right">
-                    <a href="#">Back to top</a>
-                </p>
-                <p>Copyright to &copy; Akif</p>
-                <p>
-                    Project's Github available on: 
-                    <a href="https://github.com/akifkartal03/sandwich-recipe-app">
-                         Visit our github page
-                    </a>
-                    .
-                </p>
-            </Container>
-        </footer>
-    );
+  return (
+    <footer className="text-muted">
+      <Container>
+        <p className="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Copyright to &copy; Akif</p>
+        <p>
+          Project's Github available on:
+          <a href="https://github.com/akifkartal03/sandwich-recipe-app">
+            Visit our github page
+          </a>
+          .
+        </p>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,54 @@
-import React from 'react';
+import React from "react";
 import {
-    Col,
-    Collapse,
-    Container,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    NavLink,
-    Row
-} from 'reactstrap';
+  Col,
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  NavLink,
+  Row,
+} from "reactstrap";
 
 const Header = ({ collapsed, toggleNavbar, socialLinks }) => {
-    return (
-        <header>
-            <Navbar color="dark" dark>
-                <Container>
-                    <NavbarBrand
-                        href="/"
-                        className="d-flex align-items-center mr-auto"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="mr-2"
-                        />
-                        <a class="navbar-brand" href="#">
-                            <img
-                                src="https://i.ibb.co/12pWKWZ/Sandwich2.png"
-                                width="70"
-                                height="50"
-                                class="d-inline-block align-top"
-                                alt=""
-                                loading="lazy"
-                            />
-                        </a>
-                        <NavLink style={{color: 'white'}} href="#">Home</NavLink>
-                        <NavLink style={{color: 'white'}} href="#">All Recipies</NavLink>
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-        </header>
-    );
+  return (
+    <header>
+      <Navbar color="dark" dark>
+        <Container>
+          <NavbarBrand href="/" className="d-flex align-items-center mr-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2"
+            />
+            <a className="navbar-brand" href="#">
+              <img
+                src="https://i.ibb.co/12pWKWZ/Sandwich2.png"
+                width="70"
+                height="50"
+                className="d-inline-block align-top"
+                alt=""
+                loading="lazy"
+              />
+            </a>
+            <NavLink style={{ color: "white" }} href="#">
+              Home
+            </NavLink>
+            <NavLink style={{ color: "white" }} href="#">
+              All Recipies
+            </NavLink>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
